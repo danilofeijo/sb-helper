fafdebb63788d7cabaff683c97f053c1f83f0df0
Danilo Feijo <danilo.silvafs@gmail.com>
2020-09-26 22:26:11 -0300

    Moving functions file

--- sbFunctions.sh
+++ sbFunctions.sh
@@ -1,7 +1,5 @@
-#
-# Hard linked to:
-# ~/.bash_functions
-#
+#!/bin/bash
+# SB Functions
 
 #######################################################
 #                                                     #
@@ -10,70 +8,88 @@
 #######################################################
 
 #-----------------------------------------------------#
+#         Helpers                                     #
+#-----------------------------------------------------#
+
+function TEMPLATEMSG(){
+  echo
+  echo " >>  SB - $1  <<"
+  echo
+}
+
+function FINALMSG() {
+  echo
+  echo " >>  Finished --> SB!  <<"
+  echo
+}
+
+#-----------------------------------------------------#
 #         Basic Setup                                 #
 #-----------------------------------------------------#
 
+function helloWorld() {
+  ACTION="Printing Hello World"
+  TEMPLATEMSG "$ACTION"
+  echo " Hello World!"
+  FINALMSG
+}
+
 function setDefaultfolders() {
-  echo
-  echo '>>  Creating default folders --> SB!  <<'
-  echo
+  ACTION="Creating default folders"
+  TEMPLATEMSG "$ACTION"
+
   sudo mkdir ~/labspace   # A place for your studing projects
   sudo mkdir ~/workspace    # A place for your professional projects
   sudo chmod -R +777 studyspace
   sudo chmod -R +777 workspace
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
-function setDefaultHardLinks() {
-  echo
-  echo '>>  Creating default hard links for config files --> SB!  <<'
-  echo
+function createHardLinks() {
+  ACTION="Creating default hard links for config files"
+  TEMPLATEMSG "$ACTION"
+
   ln -f ~/labspace/sb-helper/live-files/.bash_aliases ~/.bash_aliases
-  ln -f ~/labspace/sb-helper/live-files/.bash_functions ~/.bash_functions
   ln -f ~/labspace/sb-helper/live-files/.bashrc ~/.bashrc
   ln -f ~/labspace/sb-helper/live-files/.zshrc ~/.zshrc
   ln -f ~/labspace/sb-helper/live-files/config ~/.config/terminator/config
   ln -f ~/labspace/sb-helper/live-files/keybindings.json ~/.config/Code/User/keybindings.json
   ln -f ~/labspace/sb-helper/live-files/projects.json ~/.config/Code/User/globalStorage/alefragnani.project-manager/projects.json
   ln -f ~/labspace/sb-helper/live-files/settings.json ~/.config/Code/User/settings.json
-  echo
-  echo '>>  Finished --> SB! <<'
-  echo
+
+  FINALMSG
 }
 
 function bitbucketSSHKeyGen () {
-  echo
-  echo '>>  Creating a new Bitbucket SSH Key --> SB!  <<'
-  echo
+  ACTION="Creating a new Bitbucket SSH Key"
+  TEMPLATEMSG "$ACTION"
+
   ssh-keygen
   echo
-  echo 'Check the keys generated ~/.ssh/'
+  echo "Check the keys generated ~/.ssh/"
   ls ~/.ssh
-  eval `ssh-agent`
+  eval "$(ssh-agent)"
   ssh-add ~/.ssh/id_rsa
   echo
   cat ~/.ssh/id_rsa.pub
   echo
-  echo '- Select and copy the key output in the clipboard'
-  echo '- Paste copied content "Personal settings >> SSH keys >> Add key >> Key (field)"'
+  echo "- Select and copy the key output in the clipboard"
+  echo "- Paste copied content 'Personal settings >> SSH keys >> Add key >> Key (field)'"
   echo
-  echo
-  echo '>>  Finished --> SB! <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://support.atlassian.com/bitbucket-cloud/docs/set-up-an-ssh-key/#SetupanSSHkey-ssh2
 }
 
 function disableTouch () {
-  echo
-  echo '>>  Disabling laptop touchscreen --> SB!  <<'
-  echo
-  sudo xinput set-prop 'ELAN Touchscreen' 'Device Enabled' 0
-  echo
-  echo '>>  Finished --> SB! <<'
-  echo
+  ACTION="Disabling laptop touchscreen"
+  TEMPLATEMSG "$ACTION"
+
+  sudo xinput set-prop "ELAN Touchscreen" "Device Enabled" 0
+
+  FINALMSG
   # Reference
   # http://www.cleuber.com.br/index.php/2016/03/14/como-desabilitar-tela-de-touch-screen-no-linux
 }
@@ -86,132 +102,123 @@ function disableTouch () {
 #---- >> Essential << --------------------------------#
 
 function installSnap() {
-  echo
-  echo '>>  Installing snapcraft --> SB!  <<'
-  echo
+  ACTION="Installing snapcraft"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt update
   sudo apt install snapd
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://snapcraft.io/docs/installing-snap-on-ubuntu
 }
 
 function installGDebi() {
-  echo
-  echo '>>  Installing GDebi --> SB!  <<'
-  echo
+  ACTION="Installing GDebi"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt install gdebi -y
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 function installGit() {
-  echo
-  echo '>>  Installing Git --> SB!  <<'
-  echo
+  ACTION="Installing Git"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt install git -y
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 function installVim() {
-  echo
-  echo '>>  Installing Vim --> SB!  <<'
-  echo
+  ACTION="Installing Vim"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt-get install vim -y
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 function installCurl() {
-  echo
-  echo '>>  Installing Curl --> SB!  <<'
-  echo
+  ACTION="Installing Curl"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt install curl -y
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 
 #---- >> Basic << ------------------------------------#
 
 function installZshFull() {
-  echo
-  echo '>>  Installing Zsh --> SB!  <<' # Not as root user
-  echo
+  ACTION="Installing Zsh"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt install zsh -y
 
-  echo
-  echo '>>  Installing Oh-my-zsh --> SB!  <<' # Not as root user
-  echo
+  ACTION="Installing Oh-my-zsh"
+  TEMPLATEMSG "$ACTION"
+
   wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
   # Copy the template .zshrc.zsh-template configuration file to the home directory .zshrc
   # cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
   # Apply settings previously set
-  source ~/.zshrc
+  source "$HOME/.zshrc"
 
-  echo
-  echo '>>  Installing zsh-better-npm-completion --> SB!  <<' # Not as root user
-  echo
+  ACTION="Installing zsh-better-npm-completion"
+  TEMPLATEMSG "$ACTION"
+
   git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
 
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+  FINALMSG
   # Reference
   # https://www.howtoforge.com/tutorial/how-to-setup-zsh-and-oh-my-zsh-on-linux/
   # https://github.com/lukechilds/zsh-better-npm-completion
 }
 
 function installNvm() {
-  echo
-  echo '>>  Installing NVM - Node Version Manager --> SB!  <<'
-  echo
+  ACTION="Installing NVM - Node Version Manager"
+  TEMPLATEMSG "$ACTION"
+
   # Requires curl
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
   bash install_nvm.sh
-  source ~/.profile
+  source "$HOME/.profile"
   nvm --version             # Verify your nvm version
   nvm install node          # "node" is an alias for the latest version
   # nvm use v12             # Change to an installed node version
   # nvm alias default v12   # Set a node version as default
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://github.com/nvm-sh/nvm#git-install
   # https://medium.com/@nbanzyme/easy-way-to-install-nvm-on-ubuntu-18-04-2cfb19ee5391
 }
 
 function installPostman() {
-  echo
-  echo '>>  Installing Postman --> SB!  <<'
-  echo
+  ACTION="Installing Postman"
+  TEMPLATEMSG "$ACTION"
+
   sudo snap install postman
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://snapcraft.io/postman
   # https://blog.bluematador.com/posts/postman-how-to-install-on-ubuntu-1604/
 }
 
 function installVSCcode() {
-  echo
-  echo '>>  Installing VSCode --> SB!  <<'
-  echo
+  ACTION="Installing VSCode"
+  TEMPLATEMSG "$ACTION"
+
   sudo snap install --classic code
 
-  echo
-  echo '>>  Installing VSCode extensions --> SB!  <<'
-  echo
-  # code --install-extension truman.autocomplate-shell
+  ACTION="Installing VSCode extensions"
+  TEMPLATEMSG "$ACTION"
+
+  code --install-extension truman.autocomplate-shell
   code --install-extension aaron-bond.better-comments
   code --install-extension CoenraadS.bracket-pair-colorizer
   code --install-extension nwhatt.chai-snippets
@@ -242,33 +249,30 @@ function installVSCcode() {
   # code --install-extension stevejpurves.cucumber
   # code --install-extension redhat.vscode-yaml
   code --install-extension gruntfuggly.todo-tree
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://code.visualstudio.com/docs/setup/linux
 }
 
 function installFlameshot() {
-  echo
-  echo '>>  Installing Flameshot --> SB!  <<'
-  echo
+  ACTION="Installing Flameshot"
+  TEMPLATEMSG "$ACTION"
+
   sudo apt install flameshot -y
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 function installPeek() {
-  echo
-  echo '>>  Installing Peek --> SB!  <<'
-  echo
+  ACTION="Installing Peek"
+  TEMPLATEMSG "$ACTION"
+
   sudo add-apt-repository ppa:peek-developers/stable -y
   sudo apt update
   sudo apt install peek -y
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://github.com/phw/peek#ubuntu
 }
@@ -277,30 +281,28 @@ function installPeek() {
 #---- >> Regular << ----------------------------------#
 
 function installSpotify() {
-  echo
-  echo '>>  Installing Spotify --> SB!  <<'
-  echo
+  ACTION="Installing Spotify"
+  TEMPLATEMSG "$ACTION"
+
   sudo snap install spotify
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 function installChrome() {
-  echo
-  echo '>>  Installing Google Chrome --> SB!  <<'
-  echo
+  ACTION="Installing Google Chrome"
+  TEMPLATEMSG "$ACTION"
+
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   sudo dpkg -i google-chrome-stable_current_amd64.deb
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
 
 function installAWSCli() {
-  echo
-  echo '>>  Installing AWS cli --> SB!  <<'
-  echo
+  ACTION="Installing AWS CLI"
+  TEMPLATEMSG "$ACTION"
+
   sudo snap install aws-cli --classic
   echo
   echo "#------------------------------------------------#"
@@ -312,19 +314,17 @@ function installAWSCli() {
   # aws configure set aws_secret_access_key default_secret_key
   # aws configure set default.region us-west-2
   # aws configure set default.ca_bundle /path/to/ca-bundle.pem
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
   # Reference
   # https://linuxhint.com/install_aws_cli_ubuntu/
 }
 
 function installNotepadqq() {
-  echo
-  echo '>>  Installing Notepad++ --> SB!  <<'
-  echo
+  ACTION="Installing Notepad++"
+  TEMPLATEMSG "$ACTION"
+
   sudo snap install notepadqq
-  echo
-  echo '>>  Finished --> SB!  <<'
-  echo
+
+  FINALMSG
 }
